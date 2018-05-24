929da01d726c7e44c84578226962e4afffa6320e
bdf2ch <bdf2ch@gmail.com>
2018-05-24 23:32:41 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { ElModule } from 'element-angular';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [{
+  path: '',
+  component: DashboardComponent,
+  children: []
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    ElModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
